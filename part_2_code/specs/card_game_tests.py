f296c4cc4e08dd35e7d09dc424bae1aee1a0c8c7
Anna Cunningham <ac232@hotmail.co.uk>
2021-02-07 17:06:42 +0000

    First test passing

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -7,3 +7,9 @@ class TestCardGame(unittest.TestCase):
     def setUp(self):
         self.card1 = Card("Hearts", 4)
         self.card2 = Card("Clubs", 8)
+        self.card_game = CardGame()
+
+    def test_check_for_ace(self):
+        self.assertEqual(False, self.card_game.check_for_ace(self.card1))
+
+        
