810e2c8f22dc7513c199026a1aa40f6cb7fcc976
Anna Cunningham <ac232@hotmail.co.uk>
2021-02-07 16:35:30 +0000

    created card instances

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,8 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.card1 = Card("Hearts", 4)
+        self.card2 = Card("Clubs", 8)
